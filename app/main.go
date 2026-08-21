9f90d97de855c2360e24b0370752a00f1b362214
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2026-08-21 19:27:21 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Print
+func echo() {
+	fmt.Println("echo")
+}
+
+var lookup = map[string]func(){
+	"echo": echo,
+}
 
 func main() {
+	var input string
+
 	fmt.Print("$ ")
+	fmt.Scanln(&input)
+
+	fn, ok := lookup[input]; 
+	if ok {
+		fn()
+	} else {
+		msg := fmt.Sprintf("{%v}: command not found", input)
+		fmt.Println(msg)
+	}
 }
