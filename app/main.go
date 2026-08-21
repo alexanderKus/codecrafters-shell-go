ccae1fe504b82b670029eceac71c7445be61e802
Aleksander Kuś <79520681+alexanderKus@users.noreply.github.com>
2026-08-21 19:45:58 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,31 +1,44 @@
 package main
 
 import (
+	"strings"
 	"fmt"
+	"os"
+	"bufio"
 )
 
-func echo() {
-	fmt.Println("echo")
+func echo(args ...string) {
+	fmt.Println(strings.Join(args, " "))
 }
 
+
 var run = true
-var lookup = map[string]func(){
+var lookup = map[string]func(args ...string){
 	"echo": echo,
-	"exit": func() {
+	"exit": func(args ...string) {
 		run = false
 	},
 }
 
 func main() {
 	var input string
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for run {
 		fmt.Print("$ ")
-		fmt.Scanln(&input)
 
-		fn, ok := lookup[input]; 
+		if !scanner.Scan() {
+			break
+    }
+
+		input = scanner.Text()
+		parts := strings.Fields(input)
+		command := parts[0]
+		args := parts[1:]
+
+		fn, ok := lookup[command]; 
 		if ok {
-			fn()
+			fn(args...)
 		} else {
 			msg := fmt.Sprintf("%v: command not found", input)
 			fmt.Println(msg)
